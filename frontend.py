95cea2933a305c0998ee0d8bd9b8469c77891dad
Joshuawadd <joshua.j.waddington@durham.ac.uk>
2020-03-12 02:25:59 +0000

    fixed frontend and backend. Just need to add functionality now

--- frontend.py
+++ frontend.py
@@ -3,12 +3,14 @@ import Pyro4
 @Pyro4.expose
 class GetName(object):
     def get_fortune(self, name):
-        return greeting_maker = Pyro4.Proxy("PYRONAME:server.backendend")
-                (greeting_maker.get_fortune(name))
+        print("Frontend")
+        greeting_maker = Pyro4.Proxy("PYRONAME:server.backend")
+        return (greeting_maker.get_fortune(name))
 
 daemon = Pyro4.Daemon()                # make a Pyro daemon
 ns = Pyro4.locateNS()                  # find the name server
-uri = daemon.register(GreetingMaker)   # register the greeting maker as a Pyro object
+uri = daemon.register(GetName)          # register the greeting maker as a Pyro object
+print(uri)  
 ns.register("server.frontend", uri)   # register the object with a name in the name server
 
 
