28aa80969dcc8002a3a779064f1f9df7ed2628ae
Joshuawadd <joshua.j.waddington@durham.ac.uk>
2020-03-12 02:00:09 +0000

    configured frontend and backend servers

--- backend.py
+++ backend.py
@@ -9,7 +9,7 @@ class GreetingMaker(object):
 daemon = Pyro4.Daemon()                # make a Pyro daemon
 ns = Pyro4.locateNS()                  # find the name server
 uri = daemon.register(GreetingMaker)   # register the greeting maker as a Pyro object
-ns.register("example.greeting", uri)   # register the object with a name in the name server
+ns.register("server.backend", uri)   # register the object with a name in the name server
 
 print("Ready.")
 daemon.requestLoop()                   # start the event loop of the server to wait for calls
